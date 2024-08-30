6400151f5f97dc7f3d9c966b9dd35a50f713ff31
HarendraNM <146410254+HarendraNM@users.noreply.github.com>
2024-08-30 14:35:15 +0530

    Update CountryNews.jsx

--- client/src/components/CountryNews.jsx
+++ client/src/components/CountryNews.jsx
@@ -18,7 +18,7 @@ function CountryNews() {
   const pageSize = 15;
   useEffect(() => {
     setIsLoading(true); 
-    fetch(`https://news-app-seven-delta.vercel.app/country/${params.iso}?page=${page}&pageSize=${pageSize}`)
+    fetch(`news-nerdy-backend.vercel.app/country/${params.iso}?page=${page}&pageSize=${pageSize}`)
     .then(response=> {
       if(response.ok){
         console.log(response.clone().json());
